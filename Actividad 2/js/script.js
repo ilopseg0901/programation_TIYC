f42c2653047f8af121d1b632cbdfa1a39ef899ce
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-01-17 11:33:50 +0100

    Add files via upload

---
+++ Actividad 2/js/script.js
@@ -0,0 +1,15 @@
+function saludar(){
+    alert("¡Hola caracola!");
+
+    carita();
+}
+
+function despedir(){
+    alert("¡Adios Caparrós!");
+
+    carita();
+}
+
+function carita(){
+    alert(":3");
+}
