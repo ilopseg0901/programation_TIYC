a02a15cea16896af6d192fd1a5e12f3b4435737b
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-01-23 14:22:48 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,6 @@
+function pedirDatos(){
+    let nombre= prompt("¿Cuál es tu nombre?");
+    let edad= prompt("¿Cuántos años tienes?");
+
+    alert("Hola " + nombre + ", veo que tienes " + edad + " años.");
+}
