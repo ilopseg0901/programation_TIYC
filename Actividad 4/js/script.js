8ec5256ee6d84e6fb30d9a55be71fea42dfab986
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-01-31 11:08:46 +0100

    terminando el script

--- Actividad 4/js/script.js
+++ Actividad 4/js/script.js
@@ -2,6 +2,6 @@ function Calcular(){
     let a= parseInt(prompt("Introduce el primer número:"), 10);
     let b= parseInt(prompt("Introduce el segundo número:"), 10);
 
-    alert("El primer número es:" + a + "El segundo número es:" + b + "La suma es:" + (a+b) + "");
+    alert("El primer número es:" + a + "El segundo número es:" + b + "La suma es:" + (a+b) + "\nLa resta es:" + (a-b) + "\nEl producto es:" + (a*b) + "\nLa división es:" + (a/b) + "\nEl resto es:" + (a%b));
 
 }
