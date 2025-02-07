7a36aa18f4c9fc4ae837bc82c28d66f2740a1760
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-02-07 11:37:54 +0100

    ;

--- Actividad 6/js/script.js
+++ Actividad 6/js/script.js
@@ -3,10 +3,10 @@ function Comprobar(){
     let modulo= n%2
 
     if (modulo==0){
-        document.getElementById("resultado").innerHTML = "El número es par"
+        document.getElementById("resultado").innerHTML = "El número es par";
     }
 
     else{
-        document.getElementById("resultado").innerHTML = "El número es impar"
+        document.getElementById("resultado").innerHTML = "El número es impar";
     }
 }
