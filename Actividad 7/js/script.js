6119fa5bb63f3800c88bf7c8c7159f902036c210
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-02-13 14:20:21 +0100

    no es un número

--- Actividad 7/js/script.js
+++ Actividad 7/js/script.js
@@ -1,41 +1,37 @@
 function Comprobar(){
     let n= parseFloat(document.getElementById("n").value , 10);
 
-    if (n>=0 && n<=10){
-        if(n<5){
-            document.getElementById("resultado").innerHTML = "Suspenso."
-            document.getElementById("resultado").style.color = "red"
-        }
-        
-        else{
-            if(n<6){
-                document.getElementById("resultado").innerHTML = "Suficiente."
-                document.getElementById("resultado").style.color = "orange"
+    if (!isNaN(n)){
+        if(n>=0 && n<=10){
+            if(n<5){
+                document.getElementById("resultado").innerHTML = "Suspenso.";
+                document.getElementById("resultado").style.color = "red";
             }
-
+            else if(n<6){
+                document.getElementById("resultado").innerHTML = "Suficiente.";
+                document.getElementById("resultado").style.color = "orange";
+            }
+            else if(n<7){
+                document.getElementById("resultado").innerHTML = "Bien.";
+                document.getElementById("resultado").style.color = "yellow";
+            }
+            else if(n<9){
+                document.getElementById("resultado").innerHTML = "Notable.";
+                document.getElementById("resultado").style.color = "green";
+            }
             else{
-                if(n<7){
-                    document.getElementById("resultado").innerHTML = "Bien."
-                    document.getElementById("resultado").style.color = "yellow"
-                }
-    
-                else{
-                    if(n<9){
-                        document.getElementById("resultado").innerHTML = "Notable."
-                        document.getElementById("resultado").style.color = "green"
-                    }
-        
-                    else{
-                        document.getElementById("resultado").innerHTML = "Sobresaliente."
-                        document.getElementById("resultado").style.color = "blue"
-                    }
-                }
+                document.getElementById("resultado").innerHTML = "Sobresaliente.";
+                document.getElementById("resultado").style.color = "blue";
             }
+        }
+        else{
+            document.getElementById("resultado").innerHTML = "La nota que has puesto no es válida.";
+            document.getElementById("resultado").style.color = "black";
         }
     }
-
-    else{
-        document.getElementById("resultado").innerHTML = "La nota que has puesto no es válida."
-        document.getElementById("resultado").style.color = "black"
+    else {
+        //No es numero
+        document.getElementById("resultado").innerHTML = "Esto no es una nota >:(";
+        document.getElementById("resultado").style.color = "red";
     }
 }
