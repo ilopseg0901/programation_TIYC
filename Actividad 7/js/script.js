c25a1126dfc9e1a0c837ddb8641193c565e53756
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-02-13 13:55:05 +0100

    COLORES

--- Actividad 7/js/script.js
+++ Actividad 7/js/script.js
@@ -4,25 +4,30 @@ function Comprobar(){
     if (n>=0 && n<=10){
         if(n<5){
             document.getElementById("resultado").innerHTML = "Suspenso."
+            document.getElementById("resultado").style.color = "red"
         }
         
         else{
             if(n<6){
                 document.getElementById("resultado").innerHTML = "Suficiente."
+                document.getElementById("resultado").style.color = "orange"
             }
 
             else{
                 if(n<7){
                     document.getElementById("resultado").innerHTML = "Bien."
+                    document.getElementById("resultado").style.color = "yellow"
                 }
     
                 else{
                     if(n<9){
                         document.getElementById("resultado").innerHTML = "Notable."
+                        document.getElementById("resultado").style.color = "green"
                     }
         
                     else{
                         document.getElementById("resultado").innerHTML = "Sobresaliente."
+                        document.getElementById("resultado").style.color = "blue"
                     }
                 }
             }
