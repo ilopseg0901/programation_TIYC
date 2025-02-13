301fb3ae0690331bc69d764e5b8402945b47137d
Inmaculada López Segovia <ilopseg0901@g.educaand.es>
2025-02-13 13:48:21 +0100

    ejercicio terminado :)

--- Actividad 7/js/script.js
+++ Actividad 7/js/script.js
@@ -1,12 +1,35 @@
 function Comprobar(){
     let n= parseFloat(document.getElementById("n").value , 10);
-    let modulo= n%2
 
-    if (modulo==0){
-        document.getElementById("resultado").innerHTML = "El número es par"
+    if (n>=0 && n<=10){
+        if(n<5){
+            document.getElementById("resultado").innerHTML = "Suspenso."
+        }
+        
+        else{
+            if(n<6){
+                document.getElementById("resultado").innerHTML = "Suficiente."
+            }
+
+            else{
+                if(n<7){
+                    document.getElementById("resultado").innerHTML = "Bien."
+                }
+    
+                else{
+                    if(n<9){
+                        document.getElementById("resultado").innerHTML = "Notable."
+                    }
+        
+                    else{
+                        document.getElementById("resultado").innerHTML = "Sobresaliente."
+                    }
+                }
+            }
+        }
     }
 
     else{
-        document.getElementById("resultado").innerHTML = "El número es impar"
+        document.getElementById("resultado").innerHTML = "La nota que has puesto no es válida."
     }
 }
